5cd16cd5676b1d0f1f389d1b532b7a63166163c9
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-14 12:08:41 +0300

    asd

--- libs/firebaseAdmin/config.ts
+++ libs/firebaseAdmin/config.ts
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      projectId: "room-8facb",
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
