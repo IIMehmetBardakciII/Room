a70170747a9a1d07592af3de84706c9155da20d6
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-14 13:20:40 +0300

    asdas

--- app/(user)/(routes)/e-learning/[videoId]/page.tsx
+++ app/(user)/(routes)/e-learning/[videoId]/page.tsx
@@ -14,13 +14,6 @@ const SingleVideoPage = async ({ params }: { params: { videoId: string } }) => {
     getCommentsNumbers(params.videoId),
   ]);
 
-  console.log({
-    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY?.substring(0, 50), // Log part of it for security
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
-      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  });
-
   return (
     //* ServerSide-ClientSide ilişkisini sağlayabilmek açısından server componentte verileri async çekip client componente yollama işlemi.
     <SingleVideoPageClient
