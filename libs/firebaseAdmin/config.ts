22482318afa85c82053915f039722550265e88c6
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-14 19:49:23 +0300

    sdsd

--- libs/firebaseAdmin/config.ts
+++ libs/firebaseAdmin/config.ts
@@ -1,22 +1,14 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  if (
-    typeof process.env.FIREBASE_CLIENT_EMAIL === "string" &&
-    typeof process.env.FIREBASE_PRIVATE_KEY === "string" &&
-    typeof process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID === "string"
-  ) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        projectId: "room-8facb",
-      }),
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    });
-  } else {
-    throw new Error("Firebase çevresel değişkenleri eksik veya geçersiz.");
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      projectId: "room-8facb",
+    }),
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  });
 }
 
 export const adminAuth = admin.auth();
