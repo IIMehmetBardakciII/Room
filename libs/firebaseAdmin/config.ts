eb74715d6e50486cccf212ccb465590e8452bba1
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-14 12:57:30 +0300

    dd

--- libs/firebaseAdmin/config.ts
+++ libs/firebaseAdmin/config.ts
@@ -9,7 +9,7 @@ if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       }),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -22,3 +22,5 @@ if (!admin.apps.length) {
 export const adminAuth = admin.auth();
 export const adminDb = admin.firestore();
 export const adminStorage = admin.storage();
+
+// ?.replace(/\\n/g, "\n")
