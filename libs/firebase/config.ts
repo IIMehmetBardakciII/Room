c7e670c0261684cb94f67135558b9c9a8d1e5cd0
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-14 19:25:51 +0300

    koy

--- libs/firebase/config.ts
+++ libs/firebase/config.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB8-s8pt1N-IF8_TungM4hfXOFhyVGGKew",
+  authDomain: "room-8facb.firebaseapp.com",
+  projectId: "room-8facb",
+  storageBucket: "room-8facb.appspot.com",
+  messagingSenderId: "313741991223",
+  appId: "1:313741991223:web:4dfdb6b5f7b52197ab8eb2",
 };
 
 // Initialize Firebase
