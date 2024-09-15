232aefea4ef8e003b4f9ddd074c4ab7600c579d4
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-15 13:37:49 +0300

    vercel deneme

--- app/(user)/(routes)/e-learning/page.tsx
+++ app/(user)/(routes)/e-learning/page.tsx
@@ -3,9 +3,11 @@ import VideosCard from "@/libs/components/organism/VideosCard";
 
 const E_learning = async () => {
   const videos = await getAllVideos();
-
+  const env = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
   return (
     <div className="grid grid-cols-1  gap-x-[60px] gap-y-[40px]  sm:grid-cols-3">
+      <h3 className="text-white">{env}</h3>
+      <h3 className="text-white">{typeof env}</h3>
       {/* {videos.map((video) => (
         <VideosCard
           key={video.id}
