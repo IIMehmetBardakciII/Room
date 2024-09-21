4c7ec7810c2e5e96664a9753aacd1131b8d9697d
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-21 22:22:24 +0300

    card ui design for premium

--- app/(user)/(routes)/(auth)/premium/page.tsx
+++ app/(user)/(routes)/(auth)/premium/page.tsx
@@ -1,4 +1,6 @@
 import { getCookies } from "@/libs/actions/Cookies";
+import { SuccessCheck } from "@/libs/components/svgs";
+import Image from "next/image";
 
 const Premium = async () => {
   const { verifiedToken: token, success } = await getCookies();
@@ -7,17 +9,60 @@ const Premium = async () => {
   const yearlyLink =
     process.env.STRIPE_YEARLY_PLAN_LINK + `?prefilled_email=${token?.email}`;
   return (
-    <div className="flex  gap-4">
-      <button className="text-white px-4 py-2 bg-hoverBlue hover:bg-defaultBlue">
-        <a target="blank" href={monthlyLink || "#"}>
-          Monthly 50₺
-        </a>
-      </button>
-      <button className="text-white px-4 py-2 bg-hoverGreen hover:bg-defaultGreen">
-        <a target="blank" href={yearlyLink || "#"}>
-          Yearly 500₺ (2 Ay Ücretsiz)
-        </a>
-      </button>
+    <div className="flex items-center h-[600px] gap-4 justify-center">
+      {/* Monthly Card */}
+
+      <a
+        target="blank"
+        href={monthlyLink || "#"}
+        className="flex flex-col  border border-sidebarNavHover h-[300px] justify-between bg-hoverBlue hover:bg-defaultBlue rounded-[5px] px-8 py-4"
+      >
+        <div className="flex flex-col gap-4">
+          <p className="text-white ">Aylık Ödeme</p>
+          <h2 className="text-white tracking-wide">
+            50₺<span>/Ay</span>
+          </h2>
+          <div className="flex items-center gap-1">
+            <SuccessCheck className="text-white w-[12px] h-[12px]" />
+            <span className="text-white">Sınırsız Eğitim içeriği</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <SuccessCheck className="text-white w-[12px] h-[12px]" />
+            <span className="text-white">Premium Videolara Erişim</span>
+          </div>
+        </div>
+        <button className="text-almostBlack px-2 py-2 rounded-[5px] bg-white">
+          Hemen Premiumlu Ol !
+        </button>
+      </a>
+      {/* Yearly Card */}
+      <a
+        target="blank"
+        href={yearlyLink || "#"}
+        className="flex flex-col  border border-sidebarNavHover h-[300px] justify-between  hover:bg-defaultBlue rounded-[5px] px-8 py-4 group"
+      >
+        <div className="flex flex-col gap-4">
+          <p className="text-white ">Yıllık Ödeme</p>
+          <h2 className="text-white tracking-wide">
+            500₺<span>/Yıl</span>
+          </h2>
+          <div className="flex items-center gap-1">
+            <SuccessCheck className="text-white w-[12px] h-[12px]" />
+            <span className="text-white font-bold">2 Ay İndirimli Ücret</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <SuccessCheck className="text-white w-[12px] h-[12px]" />
+            <span className="text-white">Sınırsız Eğitim içeriği</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <SuccessCheck className="text-white w-[12px] h-[12px]" />
+            <span className="text-white">Premium Videolara Erişim</span>
+          </div>
+        </div>
+        <button className="text-white px-2 py-2 rounded-[5px] bg-hoverBlue group-hover:bg-white group-hover:text-almostBlack">
+          Hemen Premiumlu Ol !
+        </button>
+      </a>
     </div>
   );
 };
