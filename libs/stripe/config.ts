e0f1afe9146463d9e4ff8ba90abe0923d7cfbe80
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-22 15:40:23 +0300

    dd

--- libs/stripe/config.ts
+++ libs/stripe/config.ts
@@ -1,9 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  "sk_test_51Q0pYuCvuWOWkWbgNh0DNP1qojwvzk2w1jSIA4GOqO85gjRVCUYQU1zsjLwsuhm1x1oUvE2Gn9PaZYMnQ9o6HaG100dYNv7jJW",
-  {
-    apiVersion: "2024-06-20",
-    typescript: true,
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-06-20",
+  typescript: true,
+});
