0dd282a92aae53fac1960606305f80b4dd91fe51
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-23 16:01:13 +0300

    veritabanı deneme

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -124,17 +124,17 @@
 import Stripe from "stripe";
 import { stripe } from "@/libs/stripe/config";
 import { NextRequest, NextResponse } from "next/server";
-import { initAdmin } from "@/libs/firebaseAdmin/config";
-import {
-  DocumentData,
-  QuerySnapshot,
-  Timestamp,
-} from "firebase-admin/firestore";
+// import { initAdmin } from "@/libs/firebaseAdmin/config";
+// import {
+//   DocumentData,
+//   QuerySnapshot,
+//   Timestamp,
+// } from "firebase-admin/firestore";
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
   const sig = req.headers.get("stripe-signature") as string;
-  console.log("Received Stripe signature:", sig);
+  console.log("Received Stripe signaturee:", sig);
   let event: Stripe.Event;
   console.log("Request body", body);
   try {
@@ -151,92 +151,92 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  initAdmin();
-  const admin = require("firebase-admin");
-  const adminDb = admin.firestore();
-  const collection = adminDb.collection("users");
+  // initAdmin();
+  // const admin = require("firebase-admin");
+  // const adminDb = admin.firestore();
+  // const collection = adminDb.collection("users");
 
   try {
     switch (event.type) {
       case "checkout.session.completed":
-        const session = await stripe.checkout.sessions.retrieve(
-          (event.data.object as Stripe.Checkout.Session).id,
-          {
-            expand: ["line_items"],
-          }
-        );
+        // const session = await stripe.checkout.sessions.retrieve(
+        //   (event.data.object as Stripe.Checkout.Session).id,
+        //   {
+        //     expand: ["line_items"],
+        //   }
+        // );
 
-        const customerDetails = session.customer_details;
-        if (!customerDetails || !customerDetails.email) {
-          console.error("Customer details or email not found");
-          return NextResponse.json(
-            { error: "Customer details not found" },
-            { status: 404 }
-          );
-        }
+        // const customerDetails = session.customer_details;
+        // if (!customerDetails || !customerDetails.email) {
+        //   console.error("Customer details or email not found");
+        //   return NextResponse.json(
+        //     { error: "Customer details not found" },
+        //     { status: 404 }
+        //   );
+        // }
 
-        const lineItems = session.line_items?.data || [];
-        for (const item of lineItems) {
-          const priceId = item.price?.id;
-          const isSubscription = item.price?.type === "recurring";
+        // const lineItems = session.line_items?.data || [];
+        // for (const item of lineItems) {
+        //   const priceId = item.price?.id;
+        //   const isSubscription = item.price?.type === "recurring";
 
-          if (isSubscription) {
-            let endDate: Timestamp;
-            let priceType: string;
+        //   if (isSubscription) {
+        //     let endDate: Timestamp;
+        //     let priceType: string;
 
-            if (priceId === process.env.STRIPE_YEARLY_PRICE_ID!) {
-              const currentDate = new Date();
-              currentDate.setFullYear(currentDate.getFullYear() + 1);
-              endDate = Timestamp.fromDate(currentDate);
-              priceType = "Yearly";
-            } else if (priceId === process.env.STRIPE_MONTHLY_PRICE_ID!) {
-              const currentDate = new Date();
-              currentDate.setMonth(currentDate.getMonth() + 1);
-              endDate = Timestamp.fromDate(currentDate);
-              priceType = "Monthly";
-            } else {
-              throw new Error("Invalid priceId");
-            }
+        //     if (priceId === process.env.STRIPE_YEARLY_PRICE_ID!) {
+        //       const currentDate = new Date();
+        //       currentDate.setFullYear(currentDate.getFullYear() + 1);
+        //       endDate = Timestamp.fromDate(currentDate);
+        //       priceType = "Yearly";
+        //     } else if (priceId === process.env.STRIPE_MONTHLY_PRICE_ID!) {
+        //       const currentDate = new Date();
+        //       currentDate.setMonth(currentDate.getMonth() + 1);
+        //       endDate = Timestamp.fromDate(currentDate);
+        //       priceType = "Monthly";
+        //     } else {
+        //       throw new Error("Invalid priceId");
+        //     }
 
-            const subscriptionsData = {
-              userEmail: customerDetails.email,
-              startDate: Timestamp.now(),
-              endDate,
-              priceType,
-            };
+        //     const subscriptionsData = {
+        //       userEmail: customerDetails.email,
+        //       startDate: Timestamp.now(),
+        //       endDate,
+        //       priceType,
+        //     };
 
-            try {
-              await adminDb.collection("subscriptions").add(subscriptionsData);
-            } catch (err) {
-              console.error("Error adding subscription:", err);
-            }
-          }
-        }
+        //     try {
+        //       await adminDb.collection("subscriptions").add(subscriptionsData);
+        //     } catch (err) {
+        //       console.error("Error adding subscription:", err);
+        //     }
+        //   }
+        // }
 
-        const userQuery = collection.where(
-          "email",
-          "==",
-          customerDetails.email
-        );
-        const querySnapShot: QuerySnapshot<DocumentData> =
-          await userQuery.get();
+        // const userQuery = collection.where(
+        //   "email",
+        //   "==",
+        //   customerDetails.email
+        // );
+        // const querySnapShot: QuerySnapshot<DocumentData> =
+        //   await userQuery.get();
 
-        if (querySnapShot.empty) {
-          console.log(
-            `User document not found for email: ${customerDetails.email}`
-          );
-          return NextResponse.json(
-            { error: "Kullanıcı Bulunamadı." },
-            { status: 404 }
-          );
-        }
+        // if (querySnapShot.empty) {
+        //   console.log(
+        //     `User document not found for email: ${customerDetails.email}`
+        //   );
+        //   return NextResponse.json(
+        //     { error: "Kullanıcı Bulunamadı." },
+        //     { status: 404 }
+        //   );
+        // }
 
-        const userDocId = querySnapShot.docs[0].id;
-        await collection.doc(userDocId).update({
-          userType: "Premium",
-        });
+        // const userDocId = querySnapShot.docs[0].id;
+        // await collection.doc(userDocId).update({
+        //   userType: "Premium",
+        // });
 
-        console.log("Kullanıcı tipi güncellendi:", customerDetails.email);
+        // console.log("Kullanıcı tipi güncellendi:", customerDetails.email);
         break;
 
       default:
