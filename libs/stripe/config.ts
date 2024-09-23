006562d561fd782ccd77f9e2a8b9217b4016f831
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-23 16:20:59 +0300

    ...

--- libs/stripe/config.ts
+++ libs/stripe/config.ts
@@ -1,6 +1,12 @@
 import { Stripe } from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-  typescript: true,
+// STRIPE_SECRET_KEY'nin tanımlı olup olmadığını kontrol ediyoruz
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not defined in environment variables.");
+}
+
+// Stripe nesnesini oluşturuyoruz
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-06-20", // API versiyonunu kontrol et
+  typescript: true, // TypeScript desteğini aktif hale getir
 });
