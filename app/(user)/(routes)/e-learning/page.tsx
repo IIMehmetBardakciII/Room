29c1b64c310b439338139555c208b926fc85b6e7
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-23 14:35:45 +0300

    cdasd

--- app/(user)/(routes)/e-learning/page.tsx
+++ app/(user)/(routes)/e-learning/page.tsx
@@ -3,9 +3,21 @@ import VideosCard from "@/libs/components/organism/VideosCard";
 
 const E_learning = async () => {
   const videos = await getAllVideos();
+  const STRIPE_MONTHLY_PLAN_LINK = process.env.STRIPE_MONTHLY_PLAN_LINK;
+  const STRIPE_YEARLY_PLAN_LINK = process.env.STRIPE_YEARLY_PLAN_LINK;
+  const STRIPE_MONTHLY_PRICE_ID = process.env.STRIPE_MONTHLY_PRICE_ID;
+  const STRIPE_YEARLY_PRICE_ID = process.env.STRIPE_YEARLY_PRICE_ID;
+  const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+  const STRIPE_WEBHOOK_KEY = process.env.STRIPE_WEBHOOK_KEY;
   return (
     <div className="grid grid-cols-1  gap-x-[60px] gap-y-[40px]  sm:grid-cols-3">
-      {videos.map((video) => (
+      <h1>{STRIPE_MONTHLY_PLAN_LINK}</h1>
+      <h1>{STRIPE_YEARLY_PLAN_LINK}</h1>
+      <h1>{STRIPE_MONTHLY_PRICE_ID}</h1>
+      <h1>{STRIPE_YEARLY_PRICE_ID}</h1>
+      <h1>{STRIPE_SECRET_KEY}</h1>
+      <h1>{STRIPE_WEBHOOK_KEY}</h1>
+      {/* {videos.map((video) => (
         <VideosCard
           key={video.id}
           title={video.title}
@@ -16,7 +28,7 @@ const E_learning = async () => {
           videoRate={video.rate}
           categoryId={video.categoryId}
         />
-      ))}
+      ))} */}
     </div>
   );
 };
