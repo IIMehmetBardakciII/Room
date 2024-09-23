a465c0d5057a134073339b474ecd86d046079013
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-23 15:24:33 +0300

    sorunun Kaynagi keyler degil

--- app/(user)/(routes)/e-learning/page.tsx
+++ app/(user)/(routes)/e-learning/page.tsx
@@ -3,23 +3,10 @@ import VideosCard from "@/libs/components/organism/VideosCard";
 
 const E_learning = async () => {
   const videos = await getAllVideos();
-  const STRIPE_MONTHLY_PLAN_LINK = process.env.STRIPE_MONTHLY_PLAN_LINK;
-  const STRIPE_YEARLY_PLAN_LINK = process.env.STRIPE_YEARLY_PLAN_LINK;
-  const STRIPE_MONTHLY_PRICE_ID = process.env.STRIPE_MONTHLY_PRICE_ID;
-  const STRIPE_YEARLY_PRICE_ID = process.env.STRIPE_YEARLY_PRICE_ID;
-  const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-  const STRIPE_WEBHOOK_KEY = process.env.STRIPE_WEBHOOK_KEY;
+
   return (
     <div className="grid grid-cols-1  gap-x-[60px] gap-y-[40px]  sm:grid-cols-3">
-      <div className="flex flex-col">
-        <p className="text-white">{STRIPE_MONTHLY_PLAN_LINK}</p>
-        <p className="text-white">{STRIPE_YEARLY_PLAN_LINK}</p>
-        <p className="text-white">{STRIPE_MONTHLY_PRICE_ID}</p>
-        <p className="text-white">{STRIPE_YEARLY_PRICE_ID}</p>
-        <p className="text-white">{STRIPE_SECRET_KEY}</p>
-        <p className="text-white">{STRIPE_WEBHOOK_KEY}</p>
-      </div>
-      {/* {videos.map((video) => (
+      {videos.map((video) => (
         <VideosCard
           key={video.id}
           title={video.title}
@@ -30,7 +17,7 @@ const E_learning = async () => {
           videoRate={video.rate}
           categoryId={video.categoryId}
         />
-      ))} */}
+      ))}
     </div>
   );
 };
