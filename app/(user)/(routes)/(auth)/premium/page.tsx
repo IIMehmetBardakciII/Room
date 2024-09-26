6c59c8adf8c056765a63232b8fa47d37ee33f448
MehmetBardakci <m.bardakciw@gmail.com>
2024-09-26 20:11:06 +0300

    sorun keylerde değil

--- app/(user)/(routes)/(auth)/premium/page.tsx
+++ app/(user)/(routes)/(auth)/premium/page.tsx
@@ -10,11 +10,6 @@ const Premium = async () => {
   return (
     <div className="flex items-center h-[600px] gap-4 justify-center">
       {/* Monthly Card */}
-      <div className="flex flex-col">
-        <h4 className="text-white">{process.env.STRIPE_SECRET_KEY}</h4>
-        <h4 className="text-white">{process.env.STRIPE_YEARLY_PRICE_ID}</h4>
-        <h4 className="text-white">{process.env.STRIPE_MONTHLY_PRICE_ID}</h4>
-      </div>
       <a
         target="blank"
         href={monthlyLink || "#"}
